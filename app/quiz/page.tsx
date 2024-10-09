9486da9c1ba5ff4cff355296229374cb61fbab95
Nanda Syahrasyad <nanda.s@hey.com>
2024-10-09 09:56:34 -0700

    limit quiz length

--- app/quiz/page.tsx
+++ app/quiz/page.tsx
@@ -4,8 +4,15 @@ import type { Entry } from "../_lib/dictionary";
 
 export const dynamic = "force-dynamic";
 
-export default async function QuizPage() {
-  const words = await sql<Entry[]>`select * from words order by random()`;
+export default async function QuizPage({
+  searchParams,
+}: {
+  searchParams: { limit?: string };
+}) {
+  const limit = searchParams.limit ? parseInt(searchParams.limit) : 30;
+  const words = await sql<
+    Entry[]
+  >`select * from words order by random() limit ${limit}`;
   return (
     <div className="py-8 max-w-[600px] mx-auto">
       <QuizController words={words} />
