5c6600abc1d9ff001f68e4b486292418b948a355
Nanda Syahrasyad <nanda.s@hey.com>
2025-01-07 20:39:49 -0800

    own stream

--- app/[game]/[script]/[rowNumber]/_desktop/chat.tsx
+++ app/[game]/[script]/[rowNumber]/_desktop/chat.tsx
@@ -2,9 +2,12 @@
 
 import { useChat } from "ai/react";
 import clsx from "clsx";
+import { useState } from "react";
 
 export function Chat({ sentence }: { sentence: string }) {
-  const { messages, input, handleInputChange, handleSubmit } = useChat();
+  const [messages, setMessages] = useState<
+    { id: string; role: string; content: string }[]
+  >([]);
   return (
     <div className="flex flex-col h-full text-sm">
       <div className="divide-y divide-dashed divide-sand-6">
@@ -22,17 +25,44 @@ export function Chat({ sentence }: { sentence: string }) {
       </div>
       <form
         className="mt-auto"
-        onSubmit={(evt) => {
-          handleSubmit(evt, {
-            data: { sentence },
+        onSubmit={async (evt) => {
+          evt.preventDefault();
+
+          const formData = new FormData(evt.target as HTMLFormElement);
+          const question = formData.get("question") as string;
+          if (!question) return;
+
+          setMessages([
+            ...messages,
+            { id: crypto.randomUUID(), role: "user", content: question },
+          ]);
+
+          const response = await fetch("/api/chat", {
+            method: "POST",
+            body: JSON.stringify({
+              data: { sentence },
+              messages: [{ role: "user", content: question }],
+            }),
           });
+          if (!response.ok) return;
+          const reader = response.body
+            ?.pipeThrough(new TextDecoderStream())
+            .getReader();
+          if (!reader) return;
+          const chunks = [];
+          while (true) {
+            const { value, done } = await reader.read();
+            if (done) break;
+            console.log({ value });
+            chunks.push(JSON.parse(value));
+            console.log(chunks);
+          }
         }}
       >
         <input
           className="sticky bottom-0 w-full bg-transparent py-2 placeholder:text-sand-10"
-          value={input}
+          name="question"
           placeholder="Ask a question..."
-          onChange={handleInputChange}
         />
       </form>
     </div>
--- app/api/chat/route.tsx
+++ app/api/chat/route.tsx
@@ -1,5 +1,12 @@
-import { openai } from "@ai-sdk/openai";
 import { streamText, convertToCoreMessages } from "ai";
+import { EventSourceParserStream } from "eventsource-parser/stream";
+import { createOpenAICompatible } from "@ai-sdk/openai-compatible";
+
+const perplexity = createOpenAICompatible({
+  name: "perplexity",
+  apiKey: process.env.PERPLEXITY_API_KEY,
+  baseURL: "https://api.perplexity.ai/",
+});
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
@@ -7,11 +14,42 @@ export const maxDuration = 30;
 export async function POST(req: Request) {
   const { messages, data } = await req.json();
 
-  const result = await streamText({
-    model: openai("gpt-4o-mini"),
-    system: `Given the following sentence from the Trails in the Sky JRPG series, answer the following questions. Sentence: ${data.sentence}`,
-    messages: convertToCoreMessages(messages),
-  });
+  const options = {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${process.env.PERPLEXITY_API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      model: "llama-3.1-sonar-small-128k-online",
+      messages: [
+        {
+          role: "system",
+          content: `Given the following sentence from the Trails in the Sky JRPG series, answer the following questions. Sentence: ${data.sentence}. Be precise and concise.`,
+        },
+        ...convertToCoreMessages(messages),
+      ],
+      stream: true,
+    }),
+  };
 
-  return result.toDataStreamResponse();
+  const response = await fetch(
+    "https://api.perplexity.ai/chat/completions",
+    options
+  );
+  if (!response.body) {
+    throw new Error("No response body");
+  }
+  const stream = response.body
+    .pipeThrough(new TextDecoderStream())
+    .pipeThrough(new EventSourceParserStream())
+    .pipeThrough(
+      new TransformStream({
+        transform(chunk, controller) {
+          controller.enqueue(chunk.data);
+        },
+      })
+    );
+
+  return new Response(stream);
 }
